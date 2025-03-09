b934917c951a0c8bbfd3a8bf5a7e0fe5eb8dfc15
Sarah J Watters <69488759+Mjecuse@users.noreply.github.com>
2025-03-09 13:42:07 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
@@ -6,6 +7,7 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
+    const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -32,12 +34,11 @@ function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">.
         </a>
       </div>
+     <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
